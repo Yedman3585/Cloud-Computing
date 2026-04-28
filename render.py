a8268e37746761b9f4689fab332447bef0f6bfb2
inaizabek <inaizabek@hof-university.de>
2026-04-28 12:26:15 +0200

    created and added jinja ipv6 template

--- render.py
+++ render.py
@@ -2,14 +2,22 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 
 env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('nftables_ipv4.j2')
 
 with open('vars.yml') as f:
     data = yaml.safe_load(f)
 
-output = template.render(data)
+template_v4 = env.get_template('nftables_ipv4.j2')
+output_v4 = template_v4.render(data)
 
-with open('generated.nft', 'w') as f:
-    f.write(output)
+with open('generated_ipv4.nft', 'w') as f:
+    f.write(output_v4)
 
-print(output)
+
+template_v6 = env.get_template('nftables_ipv6.j2')
+output_v6 = template_v6.render(data)
+
+with open ('generated_ipv6.nft', 'w') as f:
+	f.write(output_v6)
+
+print("IPv4 rules:\n", output_v4)
+print("\nIPv6 rules:\n", output_v6)
