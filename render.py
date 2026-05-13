165976a8216eaf05e5f2c88093bfb56a2d3cea51
inaizabek <inaizabek@hof-university.de>
2026-05-13 13:56:04 +0200

    Hostname resolution addded to render.py and ipv4 template

--- render.py
+++ render.py
@@ -1,10 +1,25 @@
-from jinja2 import Environment, FileSystemLoader
+import socket
 import yaml
+from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
 
 with open('vars.yml') as f:
     data = yaml.safe_load(f)
+
+resolved = []
+for hostname in data.get('hosts', []):
+	entry = {'name': hostname, 'ipv4': None, 'ipv6': None}
+	try:
+		for res in socket.getaddrinfo (hostname, None):
+			if res[0].name == 'AF_INET' and not entry['ipv4']:
+				entry['ipv4'] = res[4][0]
+			if res[0].name == 'AF_INET6' and not entry['ipv6']:
+				entry['ipv6'] = res[4][0]
+	except socket.gaierror as e:
+		print(f"WARNING: cna't resolved {hostname}: {e}")
+	resolved.append(entry)
+data['resolved_hosts'] = resolved 
 
 template_v4 = env.get_template('nftables_ipv4.j2')
 output_v4 = template_v4.render(data)
